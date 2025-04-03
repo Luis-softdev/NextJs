402505269bf667b6c123a52d5affc910cfef207b
Luis Fernando R. Honorato <luisferando.honorato@gmail.com>
2025-04-02 21:26:29 -0300

    Seguindo o curso

--- src/components/Link/index.js
+++ src/components/Link/index.js
@@ -1,9 +1,9 @@
-import NextLink from 'next/link'
+import NextLink from 'next/link';
 
-export default function Link({children, href, ...props}) {
-    return(
-        <NextLink href="/faq" passHref>
-        {children}
-    </NextLink>
-    )
+export default function Link({ children, href, ...props}) {
+    return (
+        <NextLink href={href} passHref>
+            {children}
+        </NextLink>
+    );
 }
